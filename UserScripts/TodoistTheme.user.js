4a434ab66328166509fac9849b5d202b6076f814
Conlan Cesar <HeroCC@users.noreply.github.com>
2018-08-28 17:39:59 -0400

    Create TodoistTheme.user.js
    I don't like the default Todoist red theme, and I'm cheap, so write a script to change the theme to a nice blue.

---
+++ UserScripts/TodoistTheme.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Todoist Theme Force
+// @namespace    http://herocc.com/
+// @version      0.1
+// @description  Replace the todoist theme
+// @author       HeroCC
+// @match        https://todoist.com/app*
+// @grant        none
+// ==/UserScript==
+
+document.documentElement.classList.add("theme_blueberry");
+//document.documentElement.classList.remove("theme_todoist");
